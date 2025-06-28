103fe0f060e87c19b1249a913ca0cd5f3a301442
Lyxbux <lyxbux@proton.me>
2025-06-28 10:07:26 +0530

    HelpException added.

---
+++ Lyxbux.Helper/HelpException.cs
@@ -0,0 +1,9 @@
+﻿namespace Lyxbux.Helper
+{
+    public abstract class HelpException : Exception
+    {
+        public HelpException() : base() { }
+        public HelpException(string message) : base(message) { }
+        public HelpException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
